40d24a786e5b4e3ce68023442497f37cbd43ef29
Aimé <aimelie47@google.com>
2025-06-14 21:18:54 +0200

    removing Credentials

--- src/main/java/com/linked/quizbot/Constants.java
+++ src/main/java/com/linked/quizbot/Constants.java
@@ -1,26 +1,10 @@
 package com.linked.quizbot;
-import com.linked.quizbot.Credentials;
-import com.linked.quizbot.commands.list.CreateListCommand;
 
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 
 /**
  * The Constants class is the class that contains all static variables
- *  nessacery during the initiation of the Main.java.
- * 
- * This is incomplete, and requires you to write the Credentions class, 
- * that will contains the bot token and infomation on your testing guild.
- * 
- * <p>
- * package com.linked.quizbot;
- * public class Credentials {
- * public static final String 
- * 		TOKEN = "",
- * 		AUTHORID = "",
- * 		DEBUGGUILDID = "",
- * 		DEBUGCHANNELID = "";
- * }
- * </p>
+ *  necessary during the initiation of the Main.java.
  * 
  */
 public class Constants {
