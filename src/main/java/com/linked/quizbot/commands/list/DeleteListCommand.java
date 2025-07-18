03f9997fd9553543ae604eabe3806bb3cf4aa2e9
Aimé <aimelie47@google.com>
2025-07-19 00:45:05 +0200

    amend

--- src/main/java/com/linked/quizbot/commands/list/DeleteListCommand.java
+++ src/main/java/com/linked/quizbot/commands/list/DeleteListCommand.java
@@ -3,7 +3,6 @@ package com.linked.quizbot.commands.list;
 import java.util.List;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Consumer;
 
 import com.linked.quizbot.Constants;
